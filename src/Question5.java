519655766f6f55c8a2c28ed42ccc604142909dbf
Frosty <3259024151@qq.com>
2022-10-30 20:33:18 +0800

    少说也做了八九个小时，我吐了 FinalCommit before tested by Oj Signed-off-by: Frosty <3259024151@qq.com>

--- src/Question5.java
+++ src/Question5.java
@@ -3,26 +3,38 @@ import java.util.*;
 //糟糕的代码
 //一堆史山了，我tm真的吐了
 //又要找点，又要去重，又要排序，还好只有两个值可以用map然后开摆，否则我真的要紫砂了
-//能不能遍历n^2棋盘上所有点的8个方向啊，感觉这样会比我这种行扫描简单一点？这个方法期中考完再写好了
+//能不能遍历n^2的棋盘上所有点的8个方向啊，感觉这样会比我这种行扫描简单一点？这个方法期中考完再写好了
 //回退还要用kmp我不会我直接开摆了
 public class Question5 {
+
+    //注意行为x列为y
+    //感觉chessboard这些可以直接搞在这里作为类的公共变量，不用无语地传来传去
+    private static int[][] chessBoard;
+
+    //离谱啊居然还有一子多杀的鬼情况，我直接用HashSet开摆了
+    //摆不了大无语，貌似数组哈希出来居然是不一样的，我不懂了
+    //private static Set<int[]> result = new HashSet<>();
+    private static List<int[]> result = new ArrayList<>();
+
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         //思路：暴力扫描行、列、左斜、右斜
-        int[][] chessBoard = generateChessBoard(sc.nextInt());
+        chessBoard = generateChessBoard(sc.nextInt());
 
-        //离谱啊居然还有一子多杀的鬼情况，我直接用HashSet开摆了
+
         //判断与添加
-        Set<int[]> result = new HashSet<>();
-        rowCheck(chessBoard, result);
-        columnCheck(chessBoard, result);
+        rowCheck();
+        columnCheck();
+        MainDiagonal.check();
+        SubDiagonal.check();
 
         //提前返回
         if (result.isEmpty()) {
             System.out.println("-1");
             return;
         }
+
 
         //排序
         int[][] arr = new int[result.size()][2];
@@ -34,10 +46,14 @@ public class Question5 {
         }
         Arrays.sort(arr, (e1, e2) -> (e1[0] == e2[0] ? (e1[1] - e2[1]) : (e1[0] - e2[0])));//正则表达式排序
 
-
-        //打印
-        for (int[] x : arr) {//key是x,value是y（别打我写的真的很臭）
+        //打印(打印的时候跳过重复的)
+        int[] lastOne = new int[2];
+        for (int[] x : arr) {
+            if (Arrays.equals(x, lastOne)) {
+                continue;
+            }
             System.out.println((x[0] + 1) + " " + (x[1] + 1));
+            lastOne = x;
         }
     }
 
@@ -51,7 +67,7 @@ public class Question5 {
         return re;
     }
 
-    static void rowCheck(int[][] chessBoard, Set<int[]> result) {
+    static void rowCheck() {
         int size = chessBoard.length;
         for (int x = 0; x < size; x++) {//行
             List<Integer> list = checker(chessBoard[x]);
@@ -61,7 +77,8 @@ public class Question5 {
         }
 
     }
-    static void columnCheck(int[][] chessBoard, Set<int[]> result) {
+
+    static void columnCheck() {
         int size = chessBoard.length;
 
         //想不到居然是从question1抄的转置
@@ -80,6 +97,7 @@ public class Question5 {
         }
 
     }
+
     static List<Integer> checker(int[] line) {//返回有多少个y是杀点
         int size = line.length;
         List<Integer> re = new ArrayList<>();
@@ -113,6 +131,55 @@ public class Question5 {
         return re;
     }
 
+    static class MainDiagonal {
+        static public void check() {
+            for (int y = chessBoard.length - 1; y >= 0; y--) {
+                List<Integer> list = checker(split(0, y));
+                for (int i : list) {//返回的是一堆x值
+                    result.add(new int[]{i, y + i});
+                }
+            }
+            for (int x = 1; x < chessBoard.length; x++) {
+                List<Integer> list = checker(split(x, 0));
+                for (int i : list) {//返回的是一堆x值
+                    result.add(new int[]{x + i, i});
+                }
+            }
+        }
+
+        static private int[] split(int startX, int startY) {
+            int[] re = new int[chessBoard.length - Math.max(startX, startY)];
+            for (int i = 0; i < re.length; i++) {
+                re[i] = chessBoard[startX + i][startY + i];
+            }
+            return re;
+        }
+    }
+
+    static class SubDiagonal {
+        static public void check() {
+            for (int y = 0; y < chessBoard.length; y++) {
+                List<Integer> list = checker(split(0, y));
+                for (int i : list) {//返回的是一堆x值
+                    result.add(new int[]{i, y - i});
+                }
+            }
+            for (int x = 1; x < chessBoard.length; x++) {
+                List<Integer> list = checker(split(x, chessBoard.length - 1));
+                for (int i : list) {//返回的是一堆x值
+                    result.add(new int[]{i + x, chessBoard.length - 1 - i});
+                }
+            }
+        }
+
+        static private int[] split(int startX, int startY) {
+            int[] re = new int[startY - startX + 1];
+            for (int i = 0; i < re.length; i++) {
+                re[i] = chessBoard[startX + i][startY - i];
+            }
+            return re;
+        }
+    }
 
 
 }
